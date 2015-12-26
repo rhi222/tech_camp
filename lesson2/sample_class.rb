1443fac6d34abfbe0a7a5981e5c4d23111076279
Ryohei Nihiyama <ryouhei222@gmail.com>
2015-12-27 02:35:33 +0900

    directoryの整理を実施

---
+++ lesson2/sample_class.rb
@@ -0,0 +1,49 @@
+class Review
+	@@review_count = 0
+	def self.get_review_count
+		return @@review_count
+	end
+
+	def initialize
+		puts "Reviewクラスのインスタンスが生成されました"
+		@title = ""
+		@genre = ""
+		@impression = ""
+		@@review_count = @@review_count + 1
+	end
+
+	def write_review
+		puts "タイトルを入力してください"
+		@title = gets.chomp
+		puts "ジャンルを入力してください"
+		@genre = gets.chomp
+		puts "感想を入力してください"
+		@impression = gets.chomp
+	end
+
+	def show_review
+		line = "---------------------------"
+		puts "ジャンル : #{@genre}\n#{line}\n"
+		puts "タイトル : #{@title}\n#{line}\n"
+		puts "感想 : #{@impression}\n#{line}\n"
+	 end
+end
+
+while true do
+	# output menu
+	puts "書いたレビューの数： #{Review.get_review_count}"
+	puts "[0]レビューを書く"
+	puts "[1]アプリを終了"
+	input = gets.to_i
+
+	if input == 0 #write review
+		review = Review.new
+		review.write_review
+		review.show_review
+	elsif input == 1
+		exit
+	else
+		puts "invalid data\nplease input again"
+	end
+
+end
