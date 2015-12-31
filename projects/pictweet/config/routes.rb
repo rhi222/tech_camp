b70e4ac476ec6605f2838b2db4792d0af6880ebd
Ryohei Nihiyama <ryouhei222@gmail.com>
2015-12-31 21:56:18 +0900

    pic tweet開発中

---
+++ projects/pictweet/config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+	root 'tweets#index' #ルートパス対応
+	get 'tweets' => 'tweets#index' #ツイート一覧画面
+	get 'tweets/new' => 'tweets#new' #ツイート投稿画面
+	post 'tweets' => 'tweets#create' #ツイート投稿機能
+end
