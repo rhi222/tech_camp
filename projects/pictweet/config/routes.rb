9be016b000b6acbcc598aa29b37fefb326f3b302
Ryohei Nihiyama <ryouhei222@gmail.com>
2016-01-01 11:29:58 +0900

    発展学習2まで実施

--- projects/pictweet/config/routes.rb
+++ projects/pictweet/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
 	root 'tweets#index' #ルートパス対応
 	get 'tweets' => 'tweets#index' #ツイート一覧画面
 	get 'tweets/new' => 'tweets#new' #ツイート投稿画面
 	post 'tweets' => 'tweets#create' #ツイート投稿機能
+	get 'users/:id' => 'users#show' #マイページ作成
 end
