12493f51ba3d664dd376ee5ff24aa98d0f75a9f6
Ryohei Nihiyama <ryouhei222@gmail.com>
2016-01-03 18:03:13 +0900

    部分テンプレートまで実装

--- projects/nekotter/app/controllers/nekotter_controller.rb
+++ projects/nekotter/app/controllers/nekotter_controller.rb
@@ -1,4 +1,12 @@
 class NekotterController < ApplicationController
 	def index
+		@nekos = Neko.all
+	end
+
+	def new
+	end
+
+	def create
+		Neko.create(text: params[:text])
 	end
 end
--- projects/nekotter/config/routes.rb
+++ projects/nekotter/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
 	get 'nekotter' => 'nekotter#index'
+	get 'nekotter/new' => 'nekotter#new'
+	post 'nekotter' => 'nekotter#create'
 end
