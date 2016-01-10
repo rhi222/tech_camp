c5ae9023d148c3757c4c9750e303cca7d0c25865
Ryohei Nihiyama <ryouhei222@gmail.com>
2016-01-10 19:34:35 +0900

    refactoring

--- test-js/app/assets/javascripts/top.js
+++ test-js/app/assets/javascripts/top.js
@@ -1,17 +1,8 @@
-console.log("Hello world");
-
-function printHelloWithButton() {
+window.addEventListener("load", function() {
 	var btn = document.querySelector("input");
-	// ボタンをDOMオブジェクトとして取得し、変数btnに代入する
-	//
-	function printHello() {
-		console.log("Hello world again");
-	}
-	
-	btn.addEventListener("click", printHello);
-	//   // ボタンのDOMオブジェクトであるbtnに対して、
-	//   // clickイベントとprintHello関数を紐付ける仕組みであるイベントリスナを追加する
-}
 
-window.addEventListener("load", printHelloWithButton);
+	 btn.addEventListener("click", function() {
+		console.log("Hello world");
+	 });
+});
 
