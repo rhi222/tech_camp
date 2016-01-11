ca4728e6834ed691bdb2e21dd0dc8459d917a94e
Ryohei Nihiyama <ryouhei222@gmail.com>
2016-01-11 17:46:57 +0900

    回答用のモデル作成および各種モデル間のアソシエーション設定

--- profy/app/models/question.rb
+++ profy/app/models/question.rb
@@ -1,6 +1,9 @@
 class Question < ActiveRecord::Base
+	# association
 	belongs_to :user
 	belongs_to :group
+	has_many :answers
 
+	# validation
 	validates_presence_of :user_id, :text, :group_id
 end
