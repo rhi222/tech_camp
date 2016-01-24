b94ac43a7b606544e8f4be762fe45033203d5928
Ryohei Nihiyama <ryouhei222@gmail.com>
2016-01-24 17:17:26 +0900

    ビューにタイムラインのインスタンス配列を渡す

--- profy/app/controllers/top_controller.rb
+++ profy/app/controllers/top_controller.rb
@@ -1,6 +1,8 @@
 class TopController < ApplicationController
 	def index
-		@question = Question.new
-		@questions = current_user.group.questions
+		# 現在ログインしているユーザーのfeed_contentを複数取得する
+		feed_contents = current_user.group.feed_contents.includes(:content)
+		# feed_contentsからそれぞれcontentのみ取得する
+		@feed_contents = feed_contents.map(&:content)
 	end
 end
