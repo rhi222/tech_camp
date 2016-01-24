b3d95fa0d7d342e9cc2925296bda69cd5ca03470
Ryohei Nihiyama <ryouhei222@gmail.com>
2016-01-24 17:50:51 +0900

    1ページあたりに表示する件数を指定する

--- profy/app/controllers/top_controller.rb
+++ profy/app/controllers/top_controller.rb
@@ -2,8 +2,8 @@ class TopController < ApplicationController
 	def index
 		@question = Question.new
 		# 現在ログインしているユーザーのfeed_contentを複数取得する
-		feed_contents = current_user.group.feed_contents.includes(:content)
+		@feed_contents = current_user.group.feed_contents.includes(:content).page(params[:page]).per(5)
 		# feed_contentsからそれぞれcontentのみ取得する
-		@feed_contents = feed_contents.map(&:content)
+		@feed_contents_resource = @feed_contents.map(&:content)
 	end
 end
